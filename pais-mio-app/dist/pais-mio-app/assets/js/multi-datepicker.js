eb28ea70c65dc5ea00644c59ef949d54180b6e3b
Ronny Venegas <ronnyvenegas11@gmail.com>
2020-05-19 20:03:07 -0600

    First build --prod. V for testing.

---
+++ pais-mio-app/dist/pais-mio-app/assets/js/multi-datepicker.js
@@ -0,0 +1,28 @@
+
+$(function () {
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            /**'Hoy': [moment(), moment()],
+            'Ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Ultimos 7 dias': [moment().subtract(6, 'days'), moment()],
+            'Ultimos 30 dias': [moment().subtract(29, 'days'), moment()],
+            */
+            'Este mes': [moment().startOf('month'), moment().endOf('month')],
+            'Ultimo mes': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+
+        }
+    }, cb);
+
+    cb(start, end);
+
+});
