913956dda870c48476374423517db53bb9d4df0c
Daniel Ruiz <ruizaraya14@gmail.com>
2020-05-20 13:50:19 -0600

    DO para el SPE-002 Crear insumos

---
+++ Pais Mio Envasado/DO/DO_InsumoEnBodega.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class DO_InsumoEnBodega
+    {
+        DO_Insumo insumo { set; get; }
+        Int32 cantidadDisponible { set; get; }
+    }
+}
