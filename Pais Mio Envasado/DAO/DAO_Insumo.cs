0f4c649d4b2d5001bc56ddf79632849acd11d670
Daniel Ruiz <ruizaraya14@gmail.com>
2020-05-20 14:33:46 -0600

    DAO insumo

---
+++ Pais Mio Envasado/DAO/DAO_Insumo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DAO_Insumo
+    {
+        private SqlConnection conexion = new SqlConnection(DAO.Properties.Settings.Default.ConnectionString);
+        
+    }
+}
