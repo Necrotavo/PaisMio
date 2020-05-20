c3d8c0ff0162993e247c689c63f5bace6a82a022
Daniel Ruiz <ruizaraya14@gmail.com>
2020-05-20 15:59:18 -0600

    BL y Dao para SPE-002 creacion de insumos

---
+++ Pais Mio Envasado/BL/BL_Insumo.cs
@@ -0,0 +1,28 @@
+﻿using DAO;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BL_Insumo
+    {
+        public bool guardarFactura(DO_Insumo doInsumo)
+        {
+
+            //calcularTotal(factura);
+            DAO_Insumo daoInsumo = new DAO_Insumo();
+            if (daoInsumo.guardarInsumo(doInsumo) <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Pais Mio Envasado/DAO/DAO_Insumo.cs
+++ Pais Mio Envasado/DAO/DAO_Insumo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,72 @@ namespace DAO
     public class DAO_Insumo
     {
         private SqlConnection conexion = new SqlConnection(DAO.Properties.Settings.Default.ConnectionString);
-        
+
+        public int guardarInsumo(DO_Insumo insumo)
+        {
+            SqlCommand insert = new SqlCommand("INSERT INTO INSUMO (EST_HAB_ESTADO, UDM_UNIDAD, INS_NOMBRE, INS_CANT_MIN_STOCK)" +
+                "VALUES (@estado, @unidad, @nombre, @cantMinStock", conexion);
+            insert.Parameters.AddWithValue("@estado", insumo.estado.estado);
+            insert.Parameters.AddWithValue("@unidad", insumo.unidad.unidad);
+            insert.Parameters.AddWithValue("@nombre", insumo.nombre);
+            insert.Parameters.AddWithValue("@cantMinStock", insumo.cantMinStock);
+
+            try
+            {
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.Open();
+                }
+
+                if (insert.ExecuteNonQuery() > 0)
+                {
+
+                    return obtenerCodigoUltimoInsumo();
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            catch (SqlException e)
+            {
+                return 0;
+            }
+            finally
+            {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
+        public int obtenerCodigoUltimoInsumo() {
+            SqlCommand obtenerCodigo = new SqlCommand("Select INS_CODIGO from INSUMO ORDER BY INS_CODIGO DESC", conexion);
+
+            try
+            {
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.Open();
+                }
+
+                return Convert.ToInt32(obtenerCodigo.ExecuteScalar());
+
+            }
+            catch (SqlException e)
+            {
+                return 0;
+            }
+            finally
+            {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
     }
 }
