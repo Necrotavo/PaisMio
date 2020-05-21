7afdd1de8349ae5134256bcb91766149c201253c
Daniel Ruiz <ruizaraya14@gmail.com>
2020-05-20 21:54:56 -0600

    DO, DAO y BL de SPE-028 Manejo de productos

---
+++ Pais Mio Envasado/BL/BL_Bodega.cs
@@ -0,0 +1,18 @@
+﻿using DAO;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BL_Bodega
+    {
+        public bool entradaInsumos(DO_Bodega doBodega) {
+            DAO_Bodega daoBodega = new DAO_Bodega();
+            return daoBodega.entradaInsumos(doBodega);
+        }
+    }
+}
