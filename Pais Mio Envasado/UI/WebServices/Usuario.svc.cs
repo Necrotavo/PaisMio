fc8c0c61ed41e15e82bb35ee7054582f543a8310
KevinJag21 <kevin.araya@ucrso.info>
2020-05-22 17:33:34 -0600

    Borré IUsuario

--- Pais Mio Envasado/UI/WebServices/Usuario.svc.cs
+++ Pais Mio Envasado/UI/WebServices/Usuario.svc.cs
@@ -4,34 +4,25 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using DO;
-using BL;
 
-namespace WebService
+namespace UI.WebServices
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Usuario" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Usuario.svc or Usuario.svc.cs at the Solution Explorer and start debugging.
     public class Usuario : IUsuario
     {
-        public bool crearUsuario(string correo, string nombre, string apellidos, string contrasena, string tipo)
+        public void DoWork()
         {
-            if (tipo.Equals("admin")) { }
-            throw new NotImplementedException();
+
         }
 
-        public List<string> getLista()
+        public List<string> getPrueba()
         {
-            List<string> list = new List<string>();
-
+            List<String> list = new List<string>();
             list.Add("a");
             list.Add("b");
             list.Add("c");
             return list;
-        }
-
-        public string getNombre()
-        {
-            return "nombre usuario";
         }
     }
 }
