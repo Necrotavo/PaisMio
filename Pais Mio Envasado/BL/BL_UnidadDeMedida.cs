20d68811ebf808c3771b5a1504b9518019e171f0
JoJoLa <jonathan.arroyo@ucrso.info>
2020-06-04 20:14:24 -0600

    SPE-001: Manejo de unidades de medida: Faltan WS

---
+++ Pais Mio Envasado/BL/BL_UnidadDeMedida.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+using DAO;
+
+namespace BL
+{
+    public class BL_UnidadDeMedida
+    {
+        /// <summary>
+        /// Método para agregar una nueva unidad de medida.
+        /// </summary>
+        /// <param name="unidad">(String) unidad de medidad a agregar</param>
+        /// <returns>(True)si se registró el cambio. (False)si no se registró</returns>
+        public bool agregarUnidad(String unidad)
+        {
+            DAO_UnidadDeMedida daoUnidad = new DAO_UnidadDeMedida();
+            return daoUnidad.agregarUnidad(unidad);
+        }
+
+       /// <summary>
+       /// Método para mostrar las unidades de medida disponibles
+       /// </summary>
+       /// <returns>(List<String> lista de las unidades de medida.)</returns>
+        public List<String> listarUnidades()
+        {
+            DAO_UnidadDeMedida daoUnidad = new DAO_UnidadDeMedida();
+            return daoUnidad.listaUnidades();
+        }
+    }
+}
