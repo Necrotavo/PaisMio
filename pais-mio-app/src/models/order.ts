cedc62647365d0efc7b91d6cf2c5995c99350ef8
JoJoLa <jonathan.arroyo@ucrso.info>
2020-06-06 19:24:15 -0600

    Modelos: Producto, Producto en Orden y Pedido.

--- pais-mio-app/src/models/order.ts
+++ pais-mio-app/src/models/order.ts
@@ -1,3 +1,21 @@
+import { ProductInOrder } from './productInOrder';
 export class Order {
-    id: string;
+    codigo: number;
+    cedulaCliente: String;
+    correoAdminIngreso : String;
+    correoAdminDespacho : String;
+    estado: String;
+    fecheIngreso : String;
+    fechaDespacho : String;
+    listaProductos : ProductInOrder[];
+
+    constructor(codigo : number, cedulaCliente: String, correoAdminIngreso: String, listaProductos: ProductInOrder[]){
+
+        this.codigo = codigo;
+        this.cedulaCliente = cedulaCliente;
+        this.correoAdminIngreso = correoAdminIngreso;
+        this.listaProductos = listaProductos;
+
+    }
+
 }
--- pais-mio-app/src/models/product.ts
+++ pais-mio-app/src/models/product.ts
@@ -1,3 +1,14 @@
 export class Product {
-    id: string;
+    codigo: number;
+    nombre: String;
+    descripcion: String;
+    estado: String;
+
+    constructor (codigo : number, nombre: String, descripcion: String, estado: String){
+
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
 }
