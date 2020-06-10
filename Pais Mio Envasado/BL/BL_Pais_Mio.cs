0aa3d663199c5ea5dc88e7dc6bd7778854746567
JoJoLa <jonathan.arroyo@ucrso.info>
2020-06-10 14:24:17 -0600

    SPE-026: Manejo de datos País Mío:  Métodos para consultar y modificar datos. Y WS.

---
+++ Pais Mio Envasado/BL/BL_Pais_Mio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+using DAO;
+
+namespace BL
+{
+    public class BL_Pais_Mio
+    {
+        public BL_Pais_Mio()
+        {
+
+        }
+
+        /// <summary>
+        /// Método para obtener los datos generales de País Mío.
+        /// </summary>
+        /// <returns>(DO_PaisMio) objeto con los datos de la empresa.</returns>
+        public DO_PaisMio obtenerDatos()
+        {
+            DAO_Pais_Mio daoPaisMio = new DAO_Pais_Mio();
+            return daoPaisMio.obtenerDatos();
+        }
+
+        /// <summary>
+        /// Método para modificar los datos existentes de País Mío.
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool modificarDatos(DO_PaisMio datos)
+        {
+            DAO_Pais_Mio daoPaisMio = new DAO_Pais_Mio();
+            return daoPaisMio.modificarDatos(datos);
+        }
+    }
+}
---
+++ Pais Mio Envasado/WebService/WS_PaisMio.svc.cs
@@ -0,0 +1,32 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BL;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WS_PaisMio" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WS_PaisMio.svc or WS_PaisMio.svc.cs at the Solution Explorer and start debugging.
+    public class WS_PaisMio : IWS_PaisMio
+    {
+        public DO_PaisMio consultarDatos(DO_PaisMio datos)
+        {
+            BL_Pais_Mio blPaisMio = new BL_Pais_Mio();
+            return blPaisMio.obtenerDatos();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public bool modificarDatos(DO_PaisMio datos)
+        {
+            BL_Pais_Mio blPaisMio = new BL_Pais_Mio();
+            return blPaisMio.modificarDatos(datos);
+        }
+    }
+}
