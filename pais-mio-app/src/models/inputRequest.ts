5222c72680948281e01e23d39c46a4edf01fd904
Daniel Ruiz <ruizaraya14@gmail.com>
2020-06-20 00:05:03 -0600

    Detalles de solicitud casi listo

--- pais-mio-app/src/models/inputRequest.ts
+++ pais-mio-app/src/models/inputRequest.ts
@@ -10,18 +10,19 @@ export class InputRequest {
     bodega: number;
     insumosConsumo: InputQ[];
     insumosDescarte: InputQ[];
+    notas: string;
 
     constructor(codigo: number, bodega: number, codigoPedido: number,
-                insumosConsumo: InputQ[], insumosDescarte: InputQ[], operario: string,
-                admin: string, estado: string, fecha: string) {
-             this.codigo = codigo;
-             this.bodega = bodega;
-             this.codigoPedido = codigoPedido;
-             this.insumosConsumo = insumosConsumo;
-             this.insumosDescarte = insumosDescarte;
-             this.operario = operario;
-             this.estado = estado;
-             this.admin = admin;
-             this.fecha = fecha;
+        insumosConsumo: InputQ[], insumosDescarte: InputQ[], operario: string,
+        admin: string, estado: string, fecha: string) {
+        this.codigo = codigo;
+        this.bodega = bodega;
+        this.codigoPedido = codigoPedido;
+        this.insumosConsumo = insumosConsumo;
+        this.insumosDescarte = insumosDescarte;
+        this.operario = operario;
+        this.estado = estado;
+        this.admin = admin;
+        this.fecha = fecha;
     }
 }
